fdfad07e861a5bc06705bc7b891fc7227d953f3d
martinagorriaran <martina.gorriaran@gmail.com>
2022-04-08 08:23:53 -0300

    trabajoprogramacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotPageFoundComponent } from './pages/not-page-found/not-page-found.component';
+import { ServiceComponent } from './pages/service/service.component';
+import { TeamComponent } from './pages/team/team.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component: HomeComponent},
+  {path:'',component: HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'team', component: TeamComponent},
+  {path:'service', component: ServiceComponent},
+  {path:'contact', component: ContactComponent},
+  {path:'**', component: NotPageFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { TeamComponent } from './pages/team/team.component';
+import { ServiceComponent } from './pages/service/service.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NotPageFoundComponent } from './pages/not-page-found/not-page-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    TeamComponent,
+    ServiceComponent,
+    AboutComponent,
+    NotPageFoundComponent
   ],
   imports: [
     BrowserModule,
